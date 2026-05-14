b28d4137aa0f913e0191cc216bd70776669f1299
Göksun Gürel <148645864+goksungurel@users.noreply.github.com>
2026-05-14 13:19:11 +0300

    Add lab9

---
+++ lab9.cpp
@@ -0,0 +1,42 @@
+
+const int ledPins[] = {43, 44, 45, 46}; 
+
+
+const int buttonPins[] = {2, 3, 4, 5}; 
+
+const int totalItems = 4;
+
+int ledStates[] = {LOW, LOW, LOW, LOW}; 
+int lastButtonStates[] = {HIGH, HIGH, HIGH, HIGH}; 
+
+void setup() {
+  for (int i = 0; i < totalItems; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    
+
+    pinMode(buttonPins[i], INPUT_PULLUP); 
+  }
+}
+
+void loop() {
+  for (int i = 0; i < totalItems; i++) {
+    int currentButtonState = digitalRead(buttonPins[i]);
+
+   
+    if (lastButtonStates[i] == HIGH && currentButtonState == LOW) {
+     
+      if (ledStates[i] == LOW) {
+        ledStates[i] = HIGH;
+      } else {
+        ledStates[i] = LOW;
+      }
+      
+      
+      digitalWrite(ledPins[i], ledStates[i]);
+     
+      delay(50); 
+    }
+    
+    lastButtonStates[i] = currentButtonState;
+  }
+}
